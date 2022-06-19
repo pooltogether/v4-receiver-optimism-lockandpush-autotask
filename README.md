33b6b58078bf543f5988f3ec89cd660b732f2c45
Alvarez Maxence <maxence.alvarez@etu.univ-orleans.fr>
2022-06-19 23:07:46 +0200

    Create Optimism Lock And Push Autotask

---
+++ README.md
@@ -0,0 +1,13 @@
+# Receiver Optimism LockAndPush V1.1.0
+
+**Environment Variable** 
+```.sh 
+export AUTOTASK_ID=
+```
+
+# Setup
+
+1. Copy `.envrc.example` to `.envrc`
+2. Update `.envrc` with the appropriate variables
+3. Run `direnv allow`.  See [direnv](https://direnv.net)
+4. Run `yarn update` to update the autotasks.
---
+++ src/cli.ts
@@ -0,0 +1,30 @@
+import { handler } from './handler'
+
+// To run locally (this code will not be executed in Autotasks)
+if (require.main === module) {
+  const {
+    RELAYER_API_KEY,
+    RELAYER_API_SECRET,
+    ETHEREUM_MAINNET_PROVIDER_URL: ethereumMainnetProviderURL,
+    POLYGON_MAINNET_PROVIDER_URL: polygonMainnetProviderURL,
+    AVALANCHE_MAINNET_PROVIDER_URL: avalancheMainnetProviderURL,
+    OPTIMISM_MAINNET_PROVIDER_URL: optimismMainnetProviderURL
+
+  } = process.env;
+  handler({
+    apiKey: RELAYER_API_KEY,
+    apiSecret: RELAYER_API_SECRET,
+    secrets: {
+      ethereumMainnetProviderURL,
+      polygonMainnetProviderURL,
+      avalancheMainnetProviderURL,
+      optimismMainnetProviderURL
+    }
+  })
+    .then(() => process.exit(0))
+    .catch(error => { console.error(error); process.exit(1); });
+}
+
+export function main() {
+
+}
